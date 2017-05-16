dbc4a1f06c7b35f022e02832a2f5baec4fe19b74
Mouse <937279156@qq.com>
2017-05-17 00:17:01 +0800

    Update

---
+++ src/ArduinoLib.h
@@ -0,0 +1,37 @@
+#ifndef ARDUINOLIB_H
+#define ARDUINOLIB_H
+
+#define AL_HIGH 32767
+#define AL_LOW -32768
+
+
+#define AL_ANALOG_MIN 0
+#define AL_ANALOG_MAX 255
+
+class Pin{
+    private:
+    
+    Pin(){}
+    
+    int pin;
+    
+    public:
+    
+    Pin(int pin){
+        this->pin=pin;
+    }
+    
+    int read(){
+        
+    }
+    
+    void write(int value){
+        if(value==AL_HIGH)
+            digitalWrite(this->pin,HIGH);
+        else if(value==AL_LOW)
+            digitalWrite(this->pin,LOW);
+        else
+            analogeWrite(this->pin,value);
+    }
+}
+#endif
